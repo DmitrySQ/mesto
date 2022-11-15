e9ef0e121e302ef438954b5ce1510a0d94529cdd
Sarychev Dmitry <dmitry1601@mail.ru>
2022-11-15 22:43:30 +0300

    Develop: to create popup

---
+++ scripts/popup.js
@@ -0,0 +1,30 @@
+const profileName = document.querySelector(".profile__name");
+const profileDescription = document.querySelector(".profile__description");
+const editButton = document.querySelector(".profile__edit-button");
+
+const popup = document.querySelector(".popup");
+const popupButtonClose = popup.querySelector(".popup__button-close");
+const popupName = popup.querySelector(".popup__item_el_name");
+const popupDescription = popup.querySelector(".popup__item_el_description");
+const popupButtonSubmit = popup.querySelector(".popup__submit");
+
+function popupOpened() {
+  popup.classList.add("popup_opened");
+  popupName.value = profileName.textContent;
+  popupDescription.value = profileDescription.textContent;
+}
+
+function popupClosed() {
+  popup.classList.remove("popup_opened");
+}
+
+function popupSubmitHandler(evt) {
+  evt.preventDefault();
+  profileName.textContent = popupName.value;
+  profileDescription.textContent = popupDescription.value;
+  popupClosed();
+}
+
+editButton.addEventListener("click", popupOpened);
+popupButtonClose.addEventListener("click", popupClosed);
+popupButtonSubmit.addEventListener("click", popupSubmitHandler);
