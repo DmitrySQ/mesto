560b7351f8ff4d0373c1ff509c2c9347bb4a78ff
Sarychev Dmitry <dmitry1601@mail.ru>
2022-12-01 02:00:04 +0300

    dev: create animation and scripts

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,3 @@
-const profileName = document.querySelector(".profile__name");
-const profileDescription = document.querySelector(".profile__description");
-const editButton = document.querySelector(".profile__edit-button");
-// профиль
-const popup = document.querySelector(".popup");
-const addButton = document.querySelector(".profile__add-button");
-const popupButtonClose = popup.querySelector(".popup__button-close");
-const popupName = popup.querySelector(".popup__item_el_name");
-const popupDescription = popup.querySelector(".popup__item_el_description");
-const popupForm = popup.querySelector(".popup__form");
-// карточки
-const addPopup = document.getElementById("popup-add");
-const addPopupForm = document.getElementById("add-popup-form")
-const addPopupButtonClose = document.getElementById("popup-add-button-close");
-const addPopupButtonCreate = document.getElementById("add-popup-button-create");
-const addPopupTitle = document.querySelector(".popup__item_el_title");
-const addPopupLink = document.querySelector(".popup__item_el_link");
-
 const initialCards = [
   {
     name: 'Архыз',
@@ -42,6 +24,33 @@ const initialCards = [
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
   }
 ];
+
+const profileName = document.querySelector(".profile__name");
+const profileDescription = document.querySelector(".profile__description");
+const editButton = document.querySelector(".profile__edit-button");
+// профиль
+const popup = document.querySelector(".popup");
+const addButton = document.querySelector(".profile__add-button");
+const popupButtonClose = popup.querySelector(".popup__button-close");
+const popupName = popup.querySelector(".popup__item_el_name");
+const popupDescription = popup.querySelector(".popup__item_el_description");
+const popupForm = popup.querySelector(".popup__form");
+
+// карточки
+const addPopup = document.getElementById("popup-add");
+const addPopupForm = document.getElementById("add-popup-form")
+const addPopupButtonClose = document.getElementById("popup-add-button-close");
+const addPopupButtonCreate = document.getElementById("add-popup-button-create");
+const addPopupTitle = addPopup.querySelector(".popup__item_el_title");
+const addPopupLink = addPopup.querySelector(".popup__item_el_link");
+
+// картинки
+const imgPopup = document.querySelector(".popup_img");
+const popupImage = imgPopup.querySelector(".popup__image");
+const popupTitle = imgPopup.querySelector(".popup__title")
+const imgPopupButtonClose = document.getElementById("popup-img-button-close");
+
+//отрисовка карточек
 const elementsList = document.querySelector(".elements");
 const elementTemplate = document.querySelector("#element-template").content.querySelector(".element");
 function createCards (item){
@@ -55,7 +64,9 @@ function createCards (item){
   cardImage.setAttribute("src", item.link);
   cardImage.setAttribute("alt", item.name);
   cardTitle.textContent = item.name;
-  
+  cardImage.addEventListener("click", function(){
+    imgPopupBigDeal(item)
+  });
   return card;
 };
 const likeCard = function(e){
@@ -64,12 +75,12 @@ const likeCard = function(e){
 const deleteCard = function(e){
   e.target.closest(".element").remove();
 };
-
+//Перебор массива
 initialCards.forEach((item) =>{
   const element = createCards(item);
   elementsList.append(element);
 })
-
+//открытие попапов
 const popupOpened = function(item){
   item.classList.add("popup_opened");
   if (item === popup){
@@ -78,19 +89,18 @@ const popupOpened = function(item){
     return;
   }
 }
-
+//закрытие
 const popupClosed = function (item) {
   item.classList.remove("popup_opened");
 }
-
-
+//обработчик редактирования
 function popupSubmitHandler(evt) {
   evt.preventDefault();
   profileName.textContent = popupName.value;
   profileDescription.textContent = popupDescription.value;
   popupClosed(popup);
 }
-
+//обработчик добавления
 function addPopupCreateHandler(evt){
   evt.preventDefault();
   const card = {
@@ -100,6 +110,13 @@ function addPopupCreateHandler(evt){
   const element = createCards(card);
   elementsList.prepend(element);
 }
+//
+const imgPopupBigDeal = function (item){
+  popupImage.setAttribute("src", item.link);
+  popupImage.setAttribute("alt", item.link);
+  popupTitle.textContent = item.name;
+  popupOpened(imgPopup);
+}
 
 editButton.addEventListener("click", function(){
   popupOpened(popup);
@@ -108,13 +125,16 @@ addButton.addEventListener("click", function(){
   popupOpened(addPopup);
 });
 
-
 popupButtonClose.addEventListener("click", function(){
   popupClosed(popup);
 });
 addPopupButtonClose.addEventListener("click", function(){
   popupClosed(addPopup);
 });
+
+imgPopupButtonClose.addEventListener("click", function(){
+  popupClosed(imgPopup);
+})
 
 popupForm.addEventListener("submit", popupSubmitHandler);
 addPopupForm.addEventListener("submit", addPopupCreateHandler);
