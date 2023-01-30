ddb0a5891f79afff7b5f30993871b7f1b48cb6e0
Sarychev Dmitry <dmitry1601@mail.ru>
2023-01-30 17:22:25 +0300

    fix: bugfix

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
 
-import { Card } from "./components/Card.js";
-import { initialCards, validationConfig, sectionElements } from "./utils/data.js";
-import { Section } from "./components/Section.js";
-import { FormValidator } from "./components/FormValidator.js";
-import { PopupWithImage } from "./components/PopupWithImage.js";
-import { PopupWithForm } from "./components/PopupWithForm.js";
-import { UserInfo } from "./components/UserInfo.js";
+import { Card } from "../components/Card.js";
+import { initialCards, validationConfig, sectionElements } from "../utils/data.js";
+import { Section } from "../components/Section.js";
+import { FormValidator } from "../components/FormValidator.js";
+import { PopupWithImage } from "../components/PopupWithImage.js";
+import { PopupWithForm } from "../components/PopupWithForm.js";
+import { UserInfo } from "../components/UserInfo.js";
 import './index.css';
 
 // Элементы профиля
